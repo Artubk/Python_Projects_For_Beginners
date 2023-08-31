932a12fd3dfbe24651b66ddc46eb4389135b510b
Artubk <141016435+Artubk@users.noreply.github.com>
2023-08-31 16:59:04 +0700

    Add project
    Project 3

---
+++ 03. Word Cloud from a Pandas DataFrame - Generate Word Clouds/Word_Cloud_from_a_Pandas_DataFrame.py
@@ -0,0 +1,15 @@
+from wordcloud import WordCloud
+from wordcloud import ImageColorGenerator
+from wordcloud import STOPWORDS
+import matplotlib.pyplot as plt
+import pandas as pd
+data = pd.read_csv("data.csv")
+print(data.head())
+
+text = " ".join(i for i in data.text)
+stopwords = set(STOPWORDS)
+wordcloud = WordCloud(stopwords=stopwords, background_color="white").generate(text)
+plt.figure( figsize=(15,10))
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.show()
